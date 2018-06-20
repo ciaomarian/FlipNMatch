40c69e974ce0d7d062cce32477ba8cd5247a4f1e
ciaomarian <ciaomarian@gmail.com>
2018-06-20 09:55:26 -0700

    Add files via upload

---
+++ FlipNMatch/js/deck.js
@@ -0,0 +1,70 @@
+const cardData = [{
+    name: "owl",
+    image: "owl.svg",
+    id: "owl"
+}, {
+    name: "astronaut",
+    image: "astronaut.svg",
+    id: "astronaut"
+}, {
+    name: "pill",
+    image: "pill.svg",
+    id: "pill"
+}, {
+    name: "ram",
+    image: "ram.svg",
+    id: "ram"
+}, {
+    name: "xylophone",
+    image: "xylophone.svg",
+    id: "xylophone"
+}, {
+    name: "cycling",
+    image: "cycling.svg",
+    id: "cycling"
+}, {
+    name: "whistle",
+    image: "whistle.svg",
+    id: "whistle"
+}, {
+    name: "bee",
+    image: "bee.svg",
+    id: "bee"
+}, {
+    name: "lemon",
+    image: "lemon.svg",
+    id: "lemon"
+}, {
+    name: "bowling",
+    image: "bowling.svg",
+    id: "bowling"
+}, {
+    name: "cupcake",
+    image: "cupcake.svg",
+    id: "cupcake"
+}, {
+    name: "rhino",
+    image: "rhino.svg",
+    id: "rhino"
+}];
+
+const gameLevels = {
+    easy: {
+        class: "easy",
+        pairs: 6,
+        twoStar: 6,
+        oneStar: 10
+    },
+    medium: {
+        class: "medium",
+        pairs: 8,
+        twoStar: 10,
+        oneStar: 14
+    },
+    hard: {
+        class: "hard",
+        pairs: 12,
+        twoStar: 16,
+        oneStar: 24
+    }
+};
